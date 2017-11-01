c38c6eae7289c95c0d00db2f260366499a36dedc
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 23:11:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,4 +8,23 @@ class Transfer
     @status = "pending"
   end
 
+  def valid?
+    return true unless sender.valid? || receiver.valid?
+  end
+
+  def execute_transaction
+    if self.valid? && @staus = "pending"
+      @sender.withdraw(@amount)
+      @receiver.deposit(@amount)
+      @status = "succesful"
+    end
+  end
+
+  def reverse_transfer
+    if self.valid? @@ @status = "succesful"
+      @receiver.withdraw(@amount)
+      @sender.deposit(@amount)
+      @status = "reversed"
+    end
+
 end
