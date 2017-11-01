5c7bef34a9c60d77381d3839c7fb867fcdc31a32
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 23:01:00 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
+  attr_accessor :balance, :status, :name
+
+  def initialize(name)
+    @balance = 1000
+    @status = "open"
+    @name = name
+  end
+
+  def name=(name)
+    raise ArgumentError.new("Can't change the name of a bank account!")
+  end
 
 end
